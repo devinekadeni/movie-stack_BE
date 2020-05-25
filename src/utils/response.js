2ec27e0524631413c8c442ebfc1f913e985c71ae
Devin <devinekadeni@gmail.com>
2020-05-25 16:11:42 +0700

    added constants tableName and utils responses format

---
+++ src/utils/response.js
@@ -0,0 +1,34 @@
+const responseSuccess = ({ data }) => ({
+  status: 'success',
+  data,
+})
+
+const responseError = ({ errorCode, message }) => ({
+  status: 'error',
+  error: {
+    errorCode,
+    message,
+  },
+})
+
+const statusCode = {
+  success: 200,
+  error: 500,
+  notFound: 404,
+  unauthorized: 401,
+  created: 201,
+  bad: 400,
+}
+
+const errorCode = {
+  invalidInput: 'invalid_input',
+  serverError: 'server_error',
+  notFound: 'not_found',
+}
+
+module.exports = {
+  responseSuccess,
+  responseError,
+  statusCode,
+  errorCode,
+}
