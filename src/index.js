84a3cfdcbc2368647fe2326102288febff58736a
Devin <devinekadeni@gmail.com>
2020-05-26 23:28:18 +0700

    added initial setup apollo-server graphql

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 require('dotenv').config()
 const express = require('express')
 const initializeRoute = require('./routes')
+const { ApolloServer, gql } = require('apollo-server-express')
+
 const PORT = process.env.PORT || 3300
 
 const app = express()
@@ -8,9 +10,44 @@ const app = express()
 app.use(express.json())
 initializeRoute(app)
 
-app.get('/', (req, res) => {
-  res.send('Hello world')
+const Query = gql`
+  type Query {
+    hello: String
+    movie: Movie
+  }
+`
+
+const Movie = gql`
+  type Movie {
+    id: ID
+    title: String
+    actor: String
+  }
+`
+
+const typeDefs = [Query, Movie]
+
+const resolvers = {
+  Query: {
+    hello() {
+      return 'Hello GraphQL World'
+    },
+    movie() {
+      return {
+        id: 1,
+        title: 'Harry Potter',
+        actor: 'Daniel Radcliff',
+      }
+    },
+  },
+}
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
 })
+
+server.applyMiddleware({ app, path: '/graphql' })
 
 app.listen(PORT, () => {
   console.log('Server run on port ' + PORT)
