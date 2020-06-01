18b5b6ba17e4a3f96884240adfe29ddd4a055c9e
Devin <devinekadeni@gmail.com>
2020-06-02 00:24:05 +0700

    add unit test on /signin endpoint and modify some script

--- src/controllers/__test__/User.test.js
+++ src/controllers/__test__/User.test.js
@@ -1,6 +1,8 @@
 const request = require('supertest')
+const jwt = require('jsonwebtoken')
 const app = require('../../index')
 const db = require('../../db/Postgresql')
+const { statusCode } = require('../../utils/response')
 const TABLE = require('../../db/tableName')
 const { setupInitialTable, clearTable } = require('./fixtures/dbSetup')
 
@@ -13,14 +15,17 @@ afterAll(async () => {
   db.closeConnection()
 })
 
+const user1 = {
+  name: 'devin',
+  email: 'devin@gmail.com',
+  password: 'devin123',
+}
+
 describe('SIGN UP - /signup', () => {
   test('should able to register new user', async () => {
-    const newUser = {
-      name: 'devin',
-      email: 'devin@gmail.com',
-      password: 'devin123',
-    }
-    const response = await request(app).post('/user/signup').send(newUser).expect(201)
+    const response = await request(app).post('/user/signup').send(user1).expect(201)
+
+    expect(response.body.status).toBe('success')
 
     const { rows } = await db.query({
       text: `SELECT * FROM ${TABLE.USER} WHERE user_id = $1`,
@@ -28,7 +33,42 @@ describe('SIGN UP - /signup', () => {
     })
 
     expect(rows.length).toBeGreaterThan(0)
-    expect(rows[0].name).toBe(newUser.name)
-    expect(rows[0].email).toBe(newUser.email)
+    expect(rows[0].name).toBe(user1.name)
+    expect(rows[0].email).toBe(user1.email)
+  })
+})
+
+describe('SIGN IN - /signin', () => {
+  test('should not login non existing user', async () => {
+    const response = await request(app)
+      .post('/user/signin')
+      .send({ email: 'xxx@xxx.com', password: 'xxxxxx' })
+      .expect(statusCode.notFound)
+
+    expect(response.body.status).toBe('error')
+  })
+
+  test('should able to login existing user', async () => {
+    const response = await request(app)
+      .post('/user/signin')
+      .send({ email: user1.email, password: user1.password })
+      .expect(200)
+
+    const { status, data } = response.body
+
+    expect(status).toBe('success')
+    expect(jwt.verify(data.refresh_token, process.env.REFRESH_SECRET).user_id).toBe(
+      data.user_id.toString()
+    )
+    expect(jwt.verify(data.access_token, process.env.ACCESS_SECRET).user_id).toBe(
+      data.user_id.toString()
+    )
+
+    const { rows } = await db.query({
+      text: `SELECT * FROM ${TABLE.TOKEN} WHERE refresh_token = $1`,
+      values: [data.refresh_token],
+    })
+
+    expect(rows.length).toBeGreaterThan(0)
   })
 })
