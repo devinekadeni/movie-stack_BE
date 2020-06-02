22b0a98dd092580adbc30b2d171a85649a010e8e
Devin <devinekadeni@gmail.com>
2020-06-03 01:35:34 +0700

    add axios library and modify console.log on server.js

--- src/server.js
+++ src/server.js
@@ -2,5 +2,6 @@ const app = require('./index')
 const PORT = process.env.PORT || 3300
 
 app.listen(PORT, () => {
-  console.log('Server run on port ' + PORT)
+  console.log(`Server run on http://localhost:${PORT}`)
+  console.log(`Apollo server run on http://localhost:${PORT}/graphql`)
 })
