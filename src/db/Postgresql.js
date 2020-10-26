52483d1ab63e766b6d1c2886a9f4f6c7e41af5f1
Devin <devinekadeni@gmail.com>
2020-10-26 22:56:42 +0700

    postgre transaction & getClient

--- src/db/Postgresql.js
+++ src/db/Postgresql.js
@@ -24,4 +24,61 @@ module.exports = {
     }
   },
   closeConnection: () => pool.end(), // for testing needs
+  transaction: async (cb) => {
+    const client = await pool.connect();
+    const query = client.query;
+
+    client.query = (...args) => {
+      client.lastQuery = args;
+      return query.apply(client, args);
+    };
+
+    try {
+      await client.query('BEGIN');
+      await cb(client);
+      await client.query('COMMIT');
+    } catch (error) {
+      log(
+        chalk.red('Failed query transaction, rollback '),
+        chalk.magenta(client.lastQuery)
+      );
+      log(error);
+      await client.query('ROLLBACK');
+    } finally {
+      client.release();
+    }
+  },
+  getClient: async () => {
+    const client = await pool.connect();
+
+    // modifying client object to hold lastQuery properties
+    const query = client.query;
+    client.query = (...args) => {
+      client.lastQuery = args;
+      return query.apply(client, args);
+    };
+
+    const release = client.release;
+    client.release = (...args) => {
+      console.log('released by engineer');
+      clearTimeout(timeout);
+      client.query = query; // set back to its original object
+      return release.apply(client, args);
+    };
+
+    // set a time limit for transaction query 5s, otherwise will auto release
+    // to prevent other engineers forgot to invoke client.release on their transaction
+    const timeout = setTimeout(() => {
+      log(chalk.red('A client has been checked out for more than 5 seconds!'));
+      log(
+        chalk.red(
+          `The last executed query on this client was: ${chalk.magenta(client.lastQuery)}`
+        )
+      );
+
+      client.release();
+    }, 5000);
+
+    return client;
+  },
 };
