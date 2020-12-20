0ccdae779192d6bf7b8f8132bca413a7cd875255
Vinsensius Devin Ekadeni <devinekadeni@gmail.com>
2020-12-20 16:42:58 +0700

    chore: Add note

--- src/graphql/resolvers/index.js
+++ src/graphql/resolvers/index.js
@@ -2,6 +2,8 @@ const Query = require('./queryResolver');
 
 const resolvers = {
   Query,
+
+  // to fix this kind of warning https://github.com/apollographql/apollo-server/issues/1075
   QueryPagination: {
     __resolveType() {
       return false;
