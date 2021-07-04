4f58254d2733f544b506fcd3f7164b521ea0a6dd
Devin Ekadeni <devinekadeni@gmail.com>
2021-07-05 01:21:01 +0700

    chore: Remove bookmark controller from route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,8 @@
 import { Application } from 'express'
 import userRoute from './user'
-import bookmarkRoute from './bookmark'
 
 const initializeRoutes = (app: Application) => {
   app.use('/user', userRoute)
-  app.use('/bookmark', bookmarkRoute)
 }
 
 export default initializeRoutes
